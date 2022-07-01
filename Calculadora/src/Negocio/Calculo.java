a9137206b2ddad6b0f0121f44bf6b706207f0b32
JSGT9016 <105986365+JSGT9016@users.noreply.github.com>
2022-07-01 16:45:18 -0500

    Add files via upload

---
+++ Calculadora/src/Negocio/Calculo.java
@@ -0,0 +1,13 @@
+package Negocio;
+
+public class Calculo {
+	
+	public int sumar(int param1, int param2) {
+		return param1+param2;
+	}
+
+	public int dividir(int param1, int param2) {
+		return param1/param2;
+	}
+	
+}
